0df78670903f7e9f638aeafbea9b5538c8c17bca
pradeepkumar-es <createthefuture121@gmail.com>
2024-02-02 22:46:46 +0530

    new

---
+++ src/Components/ProfileComponent.jsx
@@ -0,0 +1,18 @@
+import linkedinicon from '../images/linkedin-icon.png'
+import emailicon from '../images/mail-icon.png'
+export default function ProfileComponent (props) {
+    return(
+        <>
+        <div className="card">
+            <div className="pimg"><img src={props.img} alt="profile" /></div>
+            <div className="name"><h3>{props.name}</h3></div>
+            <div className="post"><p>{props.post}</p></div>
+            <div className="team-connect">
+               <div className="linkedin"><a href={props.linkedinId}><img src={linkedinicon} alt="" /></a></div>
+               <div className="mail"><a href={props.emailId}><img src={emailicon} alt="" /></a></div>
+            </div>
+            
+        </div>
+        </>
+    )
+}
